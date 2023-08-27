b2f82293a0572bd65fddfe6d0a1152dc91e5ccdf
gilang sadewo <gilangsadewo71@gmail.com>
2023-08-27 16:27:03 +0700

    commit choose_app

---
+++ resources/views/auth/choose_app.blade.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="light" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<style>
+    #box:hover{
+		transform: scale(1.1);
+		background: #ff800a;
+		z-index: 2;
+		box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+        transition: 0.2s;
+		
+	}
+</style>
+<head>
+
+    <meta charset="utf-8" />
+    <title>Sign In | Velzon - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <link rel="shortcut icon" href="{{url('assets/images/favicon.ico')}}">
+
+    <script src="{{url('assets/js/layout.js')}}"></script>
+    <link href="{{url('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{url('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{url('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{url('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+
+    <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+        <div class="bg-overlay"></div>
+        <div class="auth-page-content overflow-hidden pt-lg-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card overflow-hidden border-0">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                        <div class="bg-overlay"></div>
+                                        <div class="position-relative h-100 d-flex flex-column">
+                                            <div class="mb-4">
+                                                <a href="index.html" class="d-block">
+                                                    <img src="{{url('assets/images/logo-light.png')}}" alt="" height="18">
+                                                </a>
+                                            </div>
+                                            <div class="mt-auto">
+                                                <div class="mb-3">
+                                                    <i class="ri-double-quotes-l display-4 text-success"></i>
+                                                </div>
+
+                                                <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
+                                                    <div class="carousel-indicators">
+                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                        <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                                    </div>
+                                                    <div class="carousel-inner text-center text-white pb-5">
+                                                        <div class="carousel-item active">
+                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
+                                                        </div>
+                                                        <div class="carousel-item">
+                                                            <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
+                                                        </div>
+                                                        <div class="carousel-item">
+                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-6">
+                                    <div class="p-lg-5 p-4">
+                                        <div id="box" class="card col-sm-6" style="background-color: #E3F2FD; border-color: transparent;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">PPDB</h5>
+                                            </div>
+                                        </div>
+                                        <div id="box" class="card col-sm-6" style="background-color: #81D4FA; border-color: transparent;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">BK</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0">&copy;
+                                <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    
+    <script src="{{url('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{url('assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{url('assets/libs/feather-icons/feather.min.js')}}"></script>
+    <script src="{{url('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+    <script src="{{url('assets/js/plugins.js')}}"></script>
+
+    <script src="{{url('assets/js/pages/password-addon.init.js')}}"></script>
+</body>
+
+
+</html>
