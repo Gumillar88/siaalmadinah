28a0ceeab1fd2127718c165ae10203cd449e3afa
Mitchell Dirks <pottsed@protonmail.ch>
2023-08-27 16:53:06 +0700

    Commit Apps row

--- resources/views/auth/choose_app.blade.php
+++ resources/views/auth/choose_app.blade.php
@@ -1,36 +1,33 @@
+<?php 
+use App\Models\Apps;
+$apps = Apps::get();
+?>
 <!doctype html>
 <html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="light" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
-
 <style>
-    #box:hover{
-		transform: scale(1.1);
-		background: #ff800a;
-		z-index: 2;
-		box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+    .apps-box:hover{
+        transform: scale(1.1);
+        background: #ff800a;
+        z-index: 2;
+        box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
         transition: 0.2s;
-		
-	}
+    }
 </style>
 <head>
-
     <meta charset="utf-8" />
     <title>Sign In | Velzon - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
     <link rel="shortcut icon" href="{{url('assets/images/favicon.ico')}}">
-
     <script src="{{url('assets/js/layout.js')}}"></script>
     <link href="{{url('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
-
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-
 <body>
-
     <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
         <div class="bg-overlay"></div>
         <div class="auth-page-content overflow-hidden pt-lg-5">
@@ -52,7 +49,6 @@
                                                 <div class="mb-3">
                                                     <i class="ri-double-quotes-l display-4 text-success"></i>
                                                 </div>
-
                                                 <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
                                                     <div class="carousel-indicators">
                                                         <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -75,29 +71,26 @@
                                         </div>
                                     </div>
                                 </div>
-                                
-                                <div class="col-lg-6">
-                                    <div class="p-lg-5 p-4">
-                                        <div id="box" class="card col-sm-6" style="background-color: #E3F2FD; border-color: transparent;">
+                                <div class="col-lg-6 row">
+                                    <div class="p-4">
+                                        <?php foreach ($apps as $row): ?>
+                                        <div class="card col-sm-6 col-xs-12 apps-box" style="background-color: #E3F2FD; border-color: transparent;">
                                             <div class="card-body">
-                                                <h5 class="card-title">PPDB</h5>
+                                                <h5 class="card-title">
+                                                    <?php echo $row->icon != null ? '<i class="'.$row->icon.'"></i>' : '' ?>
+                                                    {{$row->name}}
+                                                </h5>
                                             </div>
                                         </div>
-                                        <div id="box" class="card col-sm-6" style="background-color: #81D4FA; border-color: transparent;">
-                                            <div class="card-body">
-                                                <h5 class="card-title">BK</h5>
-                                            </div>
-                                        </div>
+                                        <?php endforeach ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
-        
         <footer class="footer">
             <div class="container">
                 <div class="row">
@@ -112,16 +105,12 @@
             </div>
         </footer>
     </div>
-    
     <script src="{{url('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{url('assets/libs/simplebar/simplebar.min.js')}}"></script>
     <script src="{{url('assets/libs/node-waves/waves.min.js')}}"></script>
     <script src="{{url('assets/libs/feather-icons/feather.min.js')}}"></script>
     <script src="{{url('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
     <script src="{{url('assets/js/plugins.js')}}"></script>
-
     <script src="{{url('assets/js/pages/password-addon.init.js')}}"></script>
 </body>
-
-
 </html>
