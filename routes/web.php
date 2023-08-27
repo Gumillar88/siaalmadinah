6d0c838873180ed05962c5490de052a89c9985ff
Mitchell Dirks <pottsed@protonmail.ch>
2023-08-27 15:21:45 +0700

    Init Commit

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return redirect('sia/login');
+    return redirect('login');
 });
-Route::get('sia/login', function () { return view('auth/login'); });
+Route::get('login', function () { return view('auth/login'); });
 // Route::get('sia/main', function () { return view('layouts/main'); });
 Route::get('dashboard', function () { return view('dashboard/dashboard'); });
 Route::get('bk', function () { return view('bk/index'); });
